ac95a2864d0ac26ef8c525837078ffc68d51192b
imaikev <47906773+imaikev@users.noreply.github.com>
2019-03-15 17:23:39 -0300

    Update main-layout.module.ts

--- frontend/src/app/shared/layouts/main-layout/main-layout.module.ts
+++ frontend/src/app/shared/layouts/main-layout/main-layout.module.ts
@@ -4,7 +4,7 @@ import {MainLayoutComponent} from './main-layout.component';
 import {CommonModule} from "@angular/common";
 import {FlexLayoutModule} from "@angular/flex-layout";
 import {NavBarComponent} from "./nav-bar/nav-bar.component";
-import {MdButtonModule, MdIconModule, MdMenuModule, MdToolbarModule} from "@angular/material";
+import {MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule} from "@angular/material";
 import {RouterModule} from "@angular/router";
 import {SideNavComponent} from "./side-nav/side-nav.component";
 
@@ -13,10 +13,10 @@ import {SideNavComponent} from "./side-nav/side-nav.component";
         CommonModule,
         RouterModule,
         FlexLayoutModule,
-        MdMenuModule,
-        MdButtonModule,
-        MdIconModule,
-        MdToolbarModule
+        MatMenuModule,
+        MatButtonModule,
+        MatIconModule,
+        MatToolbarModule
     ],
     exports: [
         MainLayoutComponent,
