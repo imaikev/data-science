941a46a114d79f27c9c3dea5bc50ba86a430ff6c
imaikev <47906773+imaikev@users.noreply.github.com>
2019-03-21 17:42:56 -0300

    Update main-layout.module.ts

--- frontend/src/app/shared/layouts/main-layout/main-layout.module.ts
+++ frontend/src/app/shared/layouts/main-layout/main-layout.module.ts
@@ -7,6 +7,7 @@ import {NavBarComponent} from "./nav-bar/nav-bar.component";
 import {MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule} from "@angular/material";
 import {MatListModule} from '@angular/material/list';
 import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
 import {RouterModule} from "@angular/router";
 import {SideNavComponent} from "./side-nav/side-nav.component";
 
@@ -19,6 +20,7 @@ import {SideNavComponent} from "./side-nav/side-nav.component";
         MatButtonModule,
         MatInputModule,
         MatListModule,
+        MatCardModule,
         MatIconModule,
         MatToolbarModule
     ],
@@ -27,6 +29,7 @@ import {SideNavComponent} from "./side-nav/side-nav.component";
         NavBarComponent,
         MatListModule,
         MatInputModule,
+        MatCardModule,
         SideNavComponent],
     declarations: [
         MainLayoutComponent,
